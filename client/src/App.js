8892a9481fb2bf5bee35218d91c714ce3512e043
Vitalii Apostolyuk <uno@MacBook-Vitalii.local>
2021-11-04 23:15:41 +0200

    pre-release

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,6 @@ function App() {
     return (
         <>
             <AppContext.Provider value={{tags, setTags}}>
-
                 <Map/>
                 <Search/>
             </AppContext.Provider>
--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,18 +1,38 @@
-import React, { useContext, useRef } from 'react';
-import { AppContext } from '../../App';
+import React, {useContext, useRef, useState} from 'react';
+import {Autocomplete} from '@react-google-maps/api';
+import {AppContext} from '../../App';
 import './Search.css';
+import {compose, withProps} from 'recompose';
+import withScriptjs from 'react-google-maps/lib/withScriptjs';
+import withGoogleMap from 'react-google-maps/lib/withGoogleMap';
 
 
+const Search = compose(
+    withProps({
+        googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_API_KEY}.exp&libraries=geometry,drawing,places`,
+        loadingElement: <div style={{height: '1%'}}/>,
+        containerElement: <div style={{height: '1px'}}/>,
+        mapElement: <div style={{height: '1%'}}/>,
+        mapId: 'c8d3fb7368eb6e72',
+    }),
+    withScriptjs,
+    withGoogleMap
+)(() => {
+    const {tags, setTags} = useContext(AppContext);
+    // eslint-disable-next-line no-unused-vars
+    const [inputOrigin, setInputOrigin] = useState(null);
 
-function InputTag(){
-    const { tags, setTags } = useContext(AppContext);
-    const tagInput = useRef(HTMLInputElement)
+    const tagInput = useRef(HTMLInputElement);
+
+    const onOriginInputChange = (e) => {
+        setInputOrigin(e.target.value);
+    };
 
     const removeTag = (i) => {
         const newTags = [...tags];
         newTags.splice(i, 1);
         setTags(newTags);
-    }
+    };
 
     const inputKeyDown = (e) => {
         const val = e.target.value;
@@ -20,7 +40,7 @@ function InputTag(){
             if (tags?.find((tag) => tag.toLowerCase() === val.toLowerCase())) {
                 return;
             }
-            setTags([ ...tags, val ]);
+            setTags([...tags, val]);
             e.target.value = '';
         } else if (e.key === 'Backspace' && !val) {
             removeTag(tags?.length - 1);
@@ -30,7 +50,8 @@ function InputTag(){
     return (
         <div className='input-tag'>
             <ul className='input-tag__tags'>
-                {tags && tags.map((tag, i) => (
+                {tags &&
+                tags.map((tag, i) => (
                     <li key={tag}>
                         {tag}
                         <button
@@ -44,20 +65,87 @@ function InputTag(){
                     </li>
                 ))}
                 <li className='input-tag__tags__input'>
-
-                    <input
-                        placeholder="Add waypoint"
-                        className='waypoints'
-                        type='text'
-                        onKeyDown={inputKeyDown}
-                        ref={tagInput}
-                    />
-
+                    <Autocomplete
+                        onPlaceChanged={(e) => {
+                            onOriginInputChange({target: tagInput.current});
+                        }}
+                    >
+                        <input
+                            placeholder='Add waypoint'
+                            className='waypoints'
+                            type='text'
+                            onKeyDown={inputKeyDown}
+                            ref={tagInput}
+                        />
+                    </Autocomplete>
                 </li>
             </ul>
         </div>
     );
-}
+});
 
-export default InputTag;
+export default Search;
 
+// import React, { useContext, useRef } from 'react';
+// import { AppContext } from '../../App';
+// import './Search.css';
+//
+//
+//
+// function InputTag(){
+//     const { tags, setTags } = useContext(AppContext);
+//     const tagInput = useRef(HTMLInputElement)
+//
+//     const removeTag = (i) => {
+//         const newTags = [...tags];
+//         newTags.splice(i, 1);
+//         setTags(newTags);
+//     }
+//
+//     const inputKeyDown = (e) => {
+//         const val = e.target.value;
+//         if (e.key === 'Enter' && val) {
+//             if (tags?.find((tag) => tag.toLowerCase() === val.toLowerCase())) {
+//                 return;
+//             }
+//             setTags([ ...tags, val ]);
+//             e.target.value = '';
+//         } else if (e.key === 'Backspace' && !val) {
+//             removeTag(tags?.length - 1);
+//         }
+//     };
+//
+//     return (
+//         <div className='input-tag'>
+//             <ul className='input-tag__tags'>
+//                 {tags && tags.map((tag, i) => (
+//                     <li key={tag}>
+//                         {tag}
+//                         <button
+//                             type='button'
+//                             onClick={() => {
+//                                 removeTag(i);
+//                             }}
+//                         >
+//                             +
+//                         </button>
+//                     </li>
+//                 ))}
+//                 <li className='input-tag__tags__input'>
+//
+//                     <input
+//                         placeholder="Add waypoint"
+//                         className='waypoints'
+//                         type='text'
+//                         onKeyDown={inputKeyDown}
+//                         ref={tagInput}
+//                     />
+//
+//                 </li>
+//             </ul>
+//         </div>
+//     );
+// }
+//
+// export default InputTag;
+//
